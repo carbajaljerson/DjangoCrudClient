e75ed5e4435ecc0ac33363e84c99c8ef15ef6be4
carbajaljerson <130612098+carbajaljerson@users.noreply.github.com>
2023-11-17 06:41:35 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pachaqtec.settings')
+    
+    settings_module = 'pachaqtec.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'pachaqtec.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+ 
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
